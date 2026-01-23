d62f9e297309b7d2ae8b7e294ff6675fecfdc974
AmirBachkouel <amir.bechkouel@sesame.com.tn>
2026-01-23 13:43:16 +0100

    Implémentation de la validation des formulaires

--- src/app/components/sign-up/sign-up.ts
+++ src/app/components/sign-up/sign-up.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css',
 })
 export class SignUp {
+  signupForm = new FormGroup ({
+    signupName: new FormControl('', {nonNullable:true}),
+    signupEmail: new FormControl('', {nonNullable: true}),
+    signupPassword: new FormControl('', {nonNullable: true}),
+  });
+  loginForm = new FormGroup ({
+    loginEmail: new FormControl('', {nonNullable: true}),
+    loginPassword: new FormControl('', {nonNullable: true}),
+  });
 
 }
